ccec4ee354d20057a104b904811529521c46122b
ivkrak <55966557+IKrakovskii@users.noreply.github.com>
2022-09-10 00:36:33 +0800

    1

--- main.py
+++ main.py
@@ -1,17 +1,5 @@
-# This is a sample Python script.
+import pandas as pd
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-from func import *
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-    while True:
-        pass
+df_contracts = pd.read_excel('Data/Контракты_Иркутск.xlsx')
+print(df_contracts)
+# update
